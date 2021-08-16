ce117e7247f4c2879b047f134188ce9991b2a9e0
nacht-wache <nachtwache1889@outlook.com>
2021-08-16 21:12:22 +0300

    add basic_tests checking

--- alg_/alg_test/basic_tests/basic_check.cpp
+++ alg_/alg_test/basic_tests/basic_check.cpp
@@ -1,5 +1,18 @@
+#include <task.hpp>
 #include <gtest/gtest.h>
 
-TEST(basic_check, test_eq) {
-  EXPECT_EQ(1, 1);
+struct BasicTestsFixture : testing::Test {
+  std::vector<ll> check;
+
+  BasicTestsFixture() {
+    check = task(5, 0, 1, 2 ,3 , 4);
+  }
+
+  ~BasicTestsFixture() = default;
+};
+
+TEST_F(BasicTestsFixture, BasicTestsName) {
+  for(ll i = 1; i < 6; ++i) {
+	EXPECT_EQ(i, check[i]);
+  }
 }
