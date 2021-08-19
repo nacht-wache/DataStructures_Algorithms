355519e03cbe9e77af45d3637b48b18dc6b40693
nacht-wache <nachtwache1889@outlook.com>
2021-08-19 19:05:36 +0300

    clang-format Google Style

--- Stack/ListBased_Template/ListBased_T.h
+++ Stack/ListBased_Template/ListBased_T.h
@@ -4,33 +4,33 @@
 #include <initializer_list>
 #include <iostream>
 
-template<typename T>
+template <typename T>
 struct Node {
-  Node<T>* prev = nullptr;
+  Node<T> *prev = nullptr;
   T key;
 };
 
-template<typename T>
+template <typename T>
 class ListBased_T {
  private:
-  Node<T>* top = nullptr;
+  Node<T> *top = nullptr;
 
  public:
   ListBased_T() = default;
   ~ListBased_T();
-  ListBased_T<T>(const std::initializer_list<T>& lst);
+  ListBased_T<T>(const std::initializer_list<T> &lst);
 
-  ListBased_T(const ListBased_T<T>& value) = delete;
-  ListBased_T<T>& operator=(const ListBased_T<T>& value) = delete;
+  ListBased_T(const ListBased_T<T> &value) = delete;
+  ListBased_T<T> &operator=(const ListBased_T<T> &value) = delete;
 
   T pop();
-  void push(const T& value);
+  void push(const T &value);
   bool empty();
 };
 
-template<typename T>
-ListBased_T<T>::ListBased_T(const std::initializer_list<T>& lst) {
-  Node<T>* ptr;
+template <typename T>
+ListBased_T<T>::ListBased_T(const std::initializer_list<T> &lst) {
+  Node<T> *ptr;
   for (typename std::initializer_list<T>::iterator it = lst.begin(); it != lst.end(); ++it) {
 	try {
 	  ptr = new Node<T>;
@@ -44,28 +44,28 @@ ListBased_T<T>::ListBased_T(const std::initializer_list<T>& lst) {
   }
 }
 
-template<typename T>
+template <typename T>
 T ListBased_T<T>::pop() {
   if (empty()) {
 	throw std::out_of_range("Underflow");
   }
-  Node<T>* ptr = top;
+  Node<T> *ptr = top;
   if (ptr->prev != nullptr) {
-    T key = top->key;
-    top = top->prev;
-    delete ptr;
+	T key = top->key;
+	top = top->prev;
+	delete ptr;
 	return key;
   } else {
-    T key = top->key;
-    delete top;
-    top = nullptr;
+	T key = top->key;
+	delete top;
+	top = nullptr;
 	return key;
   }
 }
 
-template<typename T>
-void ListBased_T<T>::push(const T& value) {
-  Node<T>* ptr;
+template <typename T>
+void ListBased_T<T>::push(const T &value) {
+  Node<T> *ptr;
   try {
 	ptr = new Node<T>;
 	ptr->prev = top;
@@ -77,12 +77,12 @@ void ListBased_T<T>::push(const T& value) {
   top = ptr;
 }
 
-template<typename T>
+template <typename T>
 ListBased_T<T>::~ListBased_T() {
   if (empty()) {
 	return;
   }
-  Node<T>* ptr = top;
+  Node<T> *ptr = top;
   while (ptr->prev != nullptr) {
 	ptr = ptr->prev;
 	delete top;
@@ -91,7 +91,7 @@ ListBased_T<T>::~ListBased_T() {
   delete top;
 }
 
-template<typename T>
+template <typename T>
 bool ListBased_T<T>::empty() {
   if (top == nullptr) {
 	return true;
